9cc4023402c43802bdf560d087c534dd941f64b0
HP 6th <iqra615>
2020-07-01 15:26:24 -0700

    Initial checkIn

---
+++ src/main/java/com/studendb/StudendbApplication.java
@@ -0,0 +1,15 @@
+package com.studendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class StudendbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudendbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studendb/StudendbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studendb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudendbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
